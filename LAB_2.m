674fb524ab3a9bc4841759e2cac48e05f89accfa
Juan Sebastian Guerrero Peña <68023761+Konat23@users.noreply.github.com>
2022-12-03 14:01:42 -0500

    Archivo de matlab para graficas

---
+++ LAB_2.m
@@ -0,0 +1,5 @@
+% Graficas del informe de 
+
+source=[0 0 1 1 0 1 1 1 0 0 1 0 0 0 0 1 1 0 1 0];
+stem(source)
+ylim([-1 2])
---
+++ Lab_2/Graficas.m
@@ -0,0 +1,7 @@
+% Graficas del informe de 
+
+source=[0 0 1 1 0 1 1 1 0 0 1 0 0 0 0 1 1 0 1 0];
+stem(source)
+ylim([-1 2])
+
+interpolador_out=[0 0 1 1 0 1 1 1 0 0 1 0 0 0 0 1 1 0 1 0]
